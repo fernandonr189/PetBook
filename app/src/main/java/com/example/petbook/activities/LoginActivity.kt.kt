e6be6ce955c2c930e4d712a339dc507b071488f0
Fernando Ismael Navarrete Ruiz <fernando.navarrete@toka.mx>
2025-04-04 16:05:05 -0600

    Completado formulario de login y creacion de cuentas con firebase

--- app/src/main/java/com/example/petbook/activities/LoginActivity.kt.kt
+++ app/src/main/java/com/example/petbook/activities/LoginActivity.kt.kt
@@ -16,12 +16,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text.input.OutputTransformation
-import androidx.compose.foundation.text.input.TextFieldBuffer
-import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -29,12 +25,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,27 +42,18 @@ import com.example.petbook.ui.theme.PetBookTheme
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
-import com.example.petbook.util.createUserAccount
+import com.example.petbook.util.signUpFirebase
 import com.example.petbook.components.FormField
+import com.example.petbook.util.loginFirebase
 
 class LoginActivity : ComponentActivity(){
 
-    private lateinit var auth: FirebaseAuth
-
-    private fun signupButtonOnClick(email: String, password: String)  {
-        createUserAccount(
-            auth,
-            email,
-            password,
-            this,
-            {
-                Toast.makeText(this, "Cuenta creada", Toast.LENGTH_SHORT).show()
-            },
-            { exception ->
-                Toast.makeText(this, exception, Toast.LENGTH_SHORT).show()
-            }
-        )
+    enum class AuthMode {
+        SIGN_UP,
+        LOG_IN
     }
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         auth = Firebase.auth
@@ -72,12 +62,10 @@ class LoginActivity : ComponentActivity(){
             .fillMaxWidth()
             .height(64.dp)
 
-        val emailTextFieldState = TextFieldState()
-        val passwordTextFieldState = TextFieldState()
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var authMode by remember { mutableStateOf(AuthMode.SIGN_UP) }
             PetBookTheme(darkTheme = false, dynamicColor =  false) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Surface(
@@ -118,14 +106,15 @@ class LoginActivity : ComponentActivity(){
                                     modifier = Modifier.padding(horizontal = 16.dp)) {
                                     Text(text = "Si ya tienes una cuenta", fontSize = 12.sp)
                                     ElevatedButton(
+                                        onClick = { authMode = AuthMode.LOG_IN },
                                         colors = ButtonColors(
                                             containerColor = MaterialTheme.colorScheme.background,
                                             contentColor = Color.Black,
                                             disabledContainerColor = MaterialTheme.colorScheme.background,
                                             disabledContentColor = Color.Black
                                         ),
-                                        shape = RectangleShape,
-                                        onClick = { Toast.makeText(applicationContext, "Unimplemented", Toast.LENGTH_SHORT).show() }) {
+                                        shape = RectangleShape
+                                    ) {
                                         Text("INICIA SESIÓN")
                                     }
                                 }
@@ -136,9 +125,7 @@ class LoginActivity : ComponentActivity(){
                                         disabledContainerColor = MaterialTheme.colorScheme.primary,
                                         disabledContentColor = Color.White
                                     ),
-                                    onClick = {
-                                        signupButtonOnClick(emailTextFieldState.text.toString(),
-                                        passwordTextFieldState.text.toString()) },
+                                    onClick = { authMode = AuthMode.SIGN_UP },
                                     shape = RoundedCornerShape(
                                         topStart = 16.dp,
                                         bottomStart = 16.dp),
@@ -152,32 +139,133 @@ class LoginActivity : ComponentActivity(){
                                     .padding(top = 16.dp),
                                 shape = RoundedCornerShape(topStart = 64.dp),
                                 color = MaterialTheme.colorScheme.primary) {
-                                Column(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(32.dp)
-                                ) {
-                                    Box(modifier = Modifier.padding(vertical = 8.dp, horizontal = 0.dp)) {
-                                        FormField(
-                                            text = "Correo electrónico:",
-                                            modifier = formFieldModifier,
-                                            inputType = KeyboardType.Email,
-                                            textFieldState = emailTextFieldState)
-                                    }
-                                    Box(modifier = Modifier.padding(vertical = 8.dp, horizontal = 0.dp)) {
-                                        FormField(
-                                            text = "Elija una contraseña:",
-                                            modifier = formFieldModifier,
-                                            inputType = KeyboardType.Password,
-                                            textFieldState = passwordTextFieldState,
-                                            isPassword = true
-                                        )
-                                    }
+                                when (authMode) {
+                                    AuthMode.LOG_IN -> LoginForm(formFieldModifier)
+                                    AuthMode.SIGN_UP -> SignupForm(formFieldModifier)
                                 }
                             }
                         }
                     }
                 }
+            }
+        }
+    }
+
+    @Composable
+    fun LoginForm(modifier: Modifier) {
+        val emailTextFieldState = TextFieldState()
+        val passwordTextFieldState = TextFieldState()
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(32.dp)
+        ) {
+            Box(modifier = Modifier.padding(vertical = 8.dp, horizontal = 0.dp)) {
+                FormField(
+                    text = "Correo electrónico:",
+                    modifier = modifier,
+                    inputType = KeyboardType.Email,
+                    textFieldState = emailTextFieldState)
+            }
+            Box(modifier = Modifier.padding(vertical = 8.dp, horizontal = 0.dp)) {
+                FormField(
+                    text = "Introduzca su contraseña:",
+                    modifier = modifier,
+                    inputType = KeyboardType.Password,
+                    textFieldState = passwordTextFieldState,
+                    isPassword = true
+                )
+            }
+            Button(onClick = {
+                if(emailTextFieldState.text.isEmpty()) {
+                    Toast.makeText(this@LoginActivity, "El correo electrónico no puede estar vacío", Toast.LENGTH_SHORT).show()
+                    return@Button
+                } else if(passwordTextFieldState.text.isEmpty()) {
+                    Toast.makeText(this@LoginActivity, "La contraseña no puede estar vacía", Toast.LENGTH_SHORT).show()
+                    return@Button
+                } else {
+                    loginFirebase(
+                        auth,
+                        emailTextFieldState.text.toString(),
+                        passwordTextFieldState.text.toString(),
+                        this@LoginActivity,
+                        {
+                            Toast.makeText(this@LoginActivity, "Bienvenido", Toast.LENGTH_SHORT).show()
+                        },
+                        { exception ->
+                            Toast.makeText(this@LoginActivity, exception, Toast.LENGTH_SHORT).show()
+                        }
+                    )
+                }
+            }) {
+                Text(text = "Continuar")
+            }
+        }
+    }
+
+    @Composable
+    fun SignupForm(modifier: Modifier) {
+        val emailTextFieldState = TextFieldState()
+        val passwordTextFieldState = TextFieldState()
+        val confirmPasswordTextFieldState = TextFieldState()
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(32.dp)
+        ) {
+            Box(modifier = Modifier.padding(vertical = 8.dp, horizontal = 0.dp)) {
+                FormField(
+                    text = "Correo electrónico:",
+                    modifier = modifier,
+                    inputType = KeyboardType.Email,
+                    textFieldState = emailTextFieldState)
+            }
+            Box(modifier = Modifier.padding(vertical = 8.dp, horizontal = 0.dp)) {
+                FormField(
+                    text = "Elija una contraseña:",
+                    modifier = modifier,
+                    inputType = KeyboardType.Password,
+                    textFieldState = passwordTextFieldState,
+                    isPassword = true
+                )
+            }
+            Box(modifier = Modifier.padding(vertical = 8.dp, horizontal = 0.dp)) {
+                FormField(
+                    text = "Confirme su contraseña:",
+                    modifier = modifier,
+                    inputType = KeyboardType.Password,
+                    textFieldState = confirmPasswordTextFieldState,
+                    isPassword = true
+                )
+            }
+            Button(onClick = {
+                if(emailTextFieldState.text.isEmpty()) {
+                    Toast.makeText(this@LoginActivity, "El correo electrónico no puede estar vacío", Toast.LENGTH_SHORT).show()
+                    return@Button
+                } else if(passwordTextFieldState.text.isEmpty()) {
+                    Toast.makeText(this@LoginActivity, "La contraseña no puede estar vacía", Toast.LENGTH_SHORT).show()
+                    return@Button
+                } else if (passwordTextFieldState.text != confirmPasswordTextFieldState.text) {
+                    Toast.makeText(this@LoginActivity, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+                    return@Button
+                } else {
+                    signUpFirebase(
+                        auth,
+                        emailTextFieldState.text.toString(),
+                        passwordTextFieldState.text.toString(),
+                        this@LoginActivity,
+                        {
+                            Toast.makeText(this@LoginActivity, "Cuenta creada", Toast.LENGTH_SHORT).show()
+                        },
+                        { exception ->
+                            Toast.makeText(this@LoginActivity, exception, Toast.LENGTH_SHORT).show()
+                        }
+                    )
+                }
+            }) {
+                Text(text = "Continuar")
             }
         }
     }
--- app/src/main/java/com/example/petbook/util/Auth.kt
+++ app/src/main/java/com/example/petbook/util/Auth.kt
@@ -3,7 +3,7 @@ package com.example.petbook.util
 import android.app.Activity
 import com.google.firebase.auth.FirebaseAuth
 
-fun createUserAccount(
+fun signUpFirebase(
     auth: FirebaseAuth,
     email: String,
     password: String,
@@ -14,6 +14,23 @@ fun createUserAccount(
         .addOnCompleteListener(context) { task ->
             if (task.isSuccessful) {
                 onUserCreated()
+            } else {
+                onFail(task.exception?.message.toString())
+            }
+        }
+}
+
+fun loginFirebase(
+    auth: FirebaseAuth,
+    email: String,
+    password: String,
+    context: Activity,
+    onLoginSuccessful: () -> Unit,
+    onFail: (String) -> Unit) {
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener(context) { task ->
+            if (task.isSuccessful) {
+                onLoginSuccessful()
             } else {
                 onFail(task.exception?.message.toString())
             }
