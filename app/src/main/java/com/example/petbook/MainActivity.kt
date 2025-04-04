7b45dcb542e0e419d74887d3b143facb58a30253
Fernando Ismael Navarrete Ruiz <fernando.navarrete@toka.mx>
2025-04-04 16:20:20 -0600

    Ahora la aplicación inicia automaticamente en el login

--- app/src/main/java/com/example/petbook/MainActivity.kt
+++ app/src/main/java/com/example/petbook/MainActivity.kt
@@ -1,22 +1,9 @@
 package com.example.petbook
-
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.petbook.activities.LoginActivity
 import com.example.petbook.ui.theme.PetBookTheme
 
@@ -26,47 +13,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PetBookTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column {
-                        Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                        GoToLoginButton()
-                    }
-                }
             }
         }
-    }
-}
-
-@Composable
-fun GoToLoginButton() {
-    val context = LocalContext.current
-    val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.StartActivityForResult() ) { result ->
-        val data = result.data
-    }
-    Button( onClick = {
-        val intent = Intent(context, LoginActivity::class.java)
-        launcher.launch(intent)
-    }){
-        Text(text = "Hello world")
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PetBookTheme {
-        Greeting("Android")
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
